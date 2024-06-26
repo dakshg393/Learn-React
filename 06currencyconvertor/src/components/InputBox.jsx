5f669851a997a03d6858656c913ce8edf9017f69
dakshg393 <142725301+dakshg393@users.noreply.github.com>
2024-06-26 23:05:55 +0530

    update app.jsx in currency convertor

--- 06currencyconvertor/src/components/InputBox.jsx
+++ 06currencyconvertor/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useId} from 'react'
 
 function InputBox({
     label,
@@ -6,19 +6,23 @@ function InputBox({
     onAmountChange,
     onCurrecyChange,
     currencyOptions =[],
-    selectCurrency ="inr",
+    selectCurrency ="usd",
     amountDisable =false,
     currecncyDisable = false,
     className = "",
 }) {
+
+    const amountInputId = useId
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label className="text-black/40 mb-2 inline-block">
+                <label className="text-black/40 mb-2 inline-block" 
+                // htmlFor={amountInputId}
+                >
                     {label}
                 </label>
                 <input
-
+                    // id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount" disabled={amountDisable} value={amount} onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
@@ -40,11 +44,12 @@ function InputBox({
                         </option>
                     })}
 
-   
                 </select>
             </div>
         </div>
-    )
+    );
 }
+
+
 
 export default InputBox;
